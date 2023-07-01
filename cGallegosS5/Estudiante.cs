f0beaeb06bf7db26ae7e835b383db623d37e4588
master <master@SMLAB306PC14_23>
2023-06-30 19:08:02 -0500

    Agregar archivos de proyecto.

---
+++ cGallegosS5/Estudiante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cGallegosS5
+{
+    public class Estudiante
+    {
+        public int codigo{ set; get; }
+        public String nombre{ set; get; }
+        public String apellido{ set; get; }
+        public int edad{ set; get; }
+    }
+}
---
+++ cGallegosS5/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace cGallegosS5
+{
+    public partial class MainPage : ContentPage
+    {
+        private string Url = "http://192.168.17.61/ws_uisrael/post.php";
+        private HttpClient cliente = new HttpClient();
+        private ObservableCollection<Estudiante> post;
+        public MainPage()
+        {
+            InitializeComponent();
+            ObtenerDatos();
+        }
+        public async void ObtenerDatos()
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<Estudiante> listaPost = JsonConvert.DeserializeObject<List<Estudiante>>(contenido);
+            post = new ObservableCollection<Estudiante>(listaPost);
+            listaEstudiantes.ItemsSource = post;
+        }
+        private async void btnMonstrar_Clicked(object sender, EventArgs e)
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<Estudiante> listaPost = JsonConvert.DeserializeObject<List<Estudiante>>(contenido);
+            post = new ObservableCollection<Estudiante>(listaPost);
+            listaEstudiantes.ItemsSource = post;
+        }
+    }
+}
